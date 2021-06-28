d3a53af8d6e1f6d36d284de495d98d412c994332
sunupdong <sunupdong@gmail.com>
2021-06-28 23:15:50 +0800

    :art: 添加公共样式

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@ import { app } from './utils/app'
 import store from './store'
 import router from './router'
 import './utils/lazy_ui'
-import './styles/index.less'
+import './styles/index.scss'
 
 app.use(store).use(router).mount('#app')
